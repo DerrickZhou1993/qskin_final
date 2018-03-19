281e55a0d86026d02fa11908395f51eb833996bf
jamesdeath123 <lhy_james@hotmail.com>
2018-03-19 17:27:35 -0400

    2nd setup where the HomePageContainer page can now be displayed.

--- app/containers/HomepageContainer.js
+++ app/containers/HomepageContainer.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
-import HomeSections from '../components/home/HomeSections'
-import { connect } from 'react-redux'
 
-export default class HomePageHandler extends Component {
+export default class HomePageContainer extends Component {
 
 	render() {
 		return (

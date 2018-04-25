e2351e8e1c55a6fce3e439cb4127c69207c35ea1
jamesdeath123 <lhy_james@hotmail.com>
2018-04-24 22:35:45 -0400

    removed unneeded codes.

--- app/pages/users/signin/SigninContainer.jsx
+++ app/pages/users/signin/SigninContainer.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import SigninForm from './SigninForm'
-import { signin } from "../../../actions/userActions"
 
 //7 todos here.
 
--- app/pages/users/signin/SigninForm.jsx
+++ app/pages/users/signin/SigninForm.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router'
 import { validateEmail } from '../../../utils/validationHelpers'
 
-//5 todos here.
+//6 todos here.
 
 export default class SigninForm extends Component {
 	constructor(props) {
@@ -13,7 +13,7 @@ export default class SigninForm extends Component {
 	}
 
 	render() {
-		//todo5: Change the following codes, so that if this.props.userSigninState.signedIn is true, return a new <div> that shows 
+		//todo6: Change the following codes, so that if this.props.userSigninState.signedIn is true, return a new <div> that shows 
 		//only a <p>sign in successful</p> page. Otherwise, return below codes.
 		return (<div>
 			<form id="signinForm" name="signupForm">
@@ -22,7 +22,7 @@ export default class SigninForm extends Component {
 					<input aria-label="email"
 						placeholder="Provide your email" 
 						type="email"
-						//todo2: add the value attribute for email filed
+						//todo2: add the value attribute for email field
 						//todo3: add onChange attribute to call the this.handleChangeEmail method with no param
 						/>
 				</div>
@@ -30,8 +30,8 @@ export default class SigninForm extends Component {
 					<input aria-label="password"
 						placeholder="Create a new password" 
 						type="password"
-						//todo3: add the value attribute for password filed
-						//todo4: add onChange attribute to call the this.handleChangePassword method with no param
+						//todo4: add the value attribute for password filed
+						//todo5: add onChange attribute to call the this.handleChangePassword method with no param
 						/>
 				</div>
 				<div>

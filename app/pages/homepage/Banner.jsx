bf101e4d22b4713bf8e46be76488c503f7539ca8
Ting <Elin@WANGs-MacBook-Pro.local>
2018-09-13 18:46:09 -0400

    adding-header-and-texts

--- app/pages/homepage/Banner.jsx
+++ app/pages/homepage/Banner.jsx
@@ -4,7 +4,12 @@ export default class Banner extends Component {
 
 	render() {
 		return (<div className="container">
-			banner place holder
+			<img id="banner" src="banner.png" alt="Qskin Forum" style="width:100%"/> 
+            <div className="container"></div>
+            <div className="words">
+                <p>You have not currently signed in.</p>
+                <p>Please<a href="/users/signin">sign in from here</a>.</p>
+            </div>
 		</div>)
 	}
 }

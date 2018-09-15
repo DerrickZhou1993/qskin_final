d153faececb5ee460084fd785f00fc9c7c7bc6fd
ting <Elin@WANGs-MacBook-Pro.local>
2018-09-15 03:54:39 -0400

    fix-image-and-text

--- app/pages/homepage/Banner.jsx
+++ app/pages/homepage/Banner.jsx
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
 import '../../stylesheets/banner.scss'
-const bannerURL='../../../assets/banner.png'
-export default class Banner extends Component {
+import banner from '../../assets/banner.png'
 
+export default class Banner extends Component {
 	render() {
 	  return (<div className="container">
-		  <img src={bannerURL} alt="Qskin Forum"/>
-		  <div className="words" >
-		    <p>You have not currently signed in.</p>
-		    <p>Please <a href="/users/signin">sign in from here</a>.</p>
-		  </div>
+		  <img src={banner} alt="Qskin Forum"/>	
+			<div className="text">	  
+		    <p>You have not currently signed in. Please <a href="/users/signin">sign in from here</a>.</p>
+			</div>
 		</div>)
 	}
 }

80d7e4942ffa56f31f4a6c8652433f62219172a4
peteryang11 <peteryang11@users.noreply.github.com>
2018-09-20 19:55:42 -0400

    Post create after signin. Add two icons ,adjust the page render, and add a .scss file (#33)
    * add the title and content to CreatPost, modify app.scss, and add bootstrap url to index, index_prod, index_qa
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete picture.png
    
    * new createpost.jsx
    
    * add two icons in CreatePost.jsx, add a new scss file createPost.scss and import two icon images

--- app/pages/posts/CreatePost.jsx
+++ app/pages/posts/CreatePost.jsx
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
+import picture from '../../assets/picture.png'
+import send from '../../assets/send.png'
+import '../../stylesheets/createPost.scss'
 
 export default class CreatePost extends Component {
 
 	render() {
 		return (<div className="container">
-			create post place holder
-		</div>)
+                    <div className="col-lg-9 col-md-9">
+                        <label>Enter the title...</label>
+                        <input className="form-control form-field-first"
+                                aria-label="title"
+                                placeholder="title" 
+                                type="text"
+                        />
+                    </div>
+                    <div className="col-lg-9 col-md-9">
+                        <label>Enter the subject content...</label>
+                        <input className="form-control form-field-first"
+                                aria-label="content"
+                                placeholder="content" 
+                                type="textarea"
+                                id="post-content-input"
+                        />
+                    </div>
+                    <div className="col-lg-9 col-md-9">
+                        <p id="end-page">
+                            <button className="btn" id="picture-button">
+                                <img src={picture} className="add-picture-btn" />
+                            </button>Add photos
+                            <button className="btn" id="send-post">
+                                <img src={send} className="send-btn" />
+                            </button>
+                        </p>
+                    </div>
+		      </div>)
 	}
 }

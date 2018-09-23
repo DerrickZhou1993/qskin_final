2b12ab2c0d7f9a0d98cd94a02cb2519e7401bc5c
Dora Chien <dorachien@Chien.local>
2018-09-23 01:33:19 -0400

    add file input ui elements and add handleChangeFiles method

--- app/pages/posts/CreatePost.jsx
+++ app/pages/posts/CreatePost.jsx
@@ -7,44 +7,56 @@ import { Link } from 'react-router'
 
 export default class CreatePost extends Component {
 
-		render() {
-			if (this.props.userSigninState.signedIn === true) {
-				return (<div className="container">
-					<div className="col-lg-9 col-md-9">
-						<label>Enter the title...</label>
-						<input className="form-control form-field-first"
-							aria-label="title"
-							placeholder="title"
-							type="text"
+	render() {
+		if (this.props.userSigninState.signedIn === true) {
+			return (<div className="container">
+				<div className="col-lg-9 col-md-9">
+					<label>Enter the title...</label>
+					<input className="form-control form-field-first"
+						aria-label="title"
+						placeholder="title"
+						type="text"
+					/>
+				</div>
+				<div className="col-lg-9 col-md-9">
+					<label>Enter the subject content...</label>
+					<input className="form-control form-field-first"
+						aria-label="content"
+						placeholder="content"
+						type="textarea"
+						id="post-content-input"
+					/>
+				</div>
+				<div className="col-lg-9 col-md-9">
+					<p id="end-page">
+						<input type="file"
+							accept="image/jpeg, image/png, image/jpeg"
+							multiple
+							onChange={this.handleChangeFiles.bind(this)}
 						/>
-					</div>
-					<div className="col-lg-9 col-md-9">
-						<label>Enter the subject content...</label>
-						<input className="form-control form-field-first"
-							aria-label="content"
-							placeholder="content"
-							type="textarea"
-							id="post-content-input"
-						/>
-					</div>
-					<div className="col-lg-9 col-md-9">
-						<p id="end-page">
-							<button className="btn" id="picture-button">
-								<img src={picture} className="add-picture-btn" />
-							</button>Add photos
-                            <button className="btn" id="send-post">
-								<img src={send} className="send-btn" />
-							</button>
-						</p>
-					</div>
-				</div>)
-			}
-			return (
-				<div>
-					<center>
-						<p>Please sign in first before creating post</p>
-						<Link to='/users/signin'>Sign in here</Link>
-					</center>
-				</div>)
+						<button className="btn" id="picture-button">
+							<img src={picture} className="add-picture-btn" />
+						</button>Add photos
+						<button className="btn" id="send-post">
+							<img src={send} className="send-btn" />
+						</button>
+					</p>
+				</div>
+			</div>)
 		}
+		return (
+			<div>
+				<center>
+					<p>Please sign in first before creating post</p>
+					<Link to='/users/signin'>Sign in here</Link>
+				</center>
+			</div>)
+	}
+
+	handleChangeFiles(event) {
+		event.preventDefault()
+		if (event.target.files.length !== 0) {
+			this.props.uploadFilesHandler(event.target.files)
+		}
+	}
 }

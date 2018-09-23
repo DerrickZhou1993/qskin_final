fda2c43c1b8581aa4b5c492e7a9548fc6031d54a
Dora Chien <dorachien@Chien.local>
2018-09-23 00:51:14 -0400

    add filesUploadState to /app/reducers/index.js

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import userSigninState from './userSigninState'
 import userSignupState from './userSignupState'
 import postState from './postState'
+import filesUploadState from './filesUploadState'
 
 export default combineReducers({
 	userSigninState,
 	userSignupState,
 	postState,
+	filesUploadState,
 })

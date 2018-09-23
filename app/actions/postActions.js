1cbaf9ab6412fce1fc6e641cf015d798bcbfd9f4
Dora Chien <dorachien@Chien.local>
2018-09-23 00:41:52 -0400

    add post action to upload photos

--- app/actions/postActions.js
+++ app/actions/postActions.js
@@ -4,6 +4,8 @@ import { postsAPI } from '../config/api'
 
 export const CREATE_POST_SUCCESSFUL = 'CREATE_POST_SUCCESSFUL'
 export const CREATE_POST_FAILED = 'CREATE_POST_FAILED'
+export const UPLOAD_FILES_SUCCESSFUL = 'UPLOAD_FILES_SUCCESSFUL'
+export const UPLOAD_FILES_FAILED = 'UPLOAD_FILES_FAILED'
 
 export function create(title, content, userId) {
     return dispatch => {
@@ -32,5 +34,30 @@ function createSuccess() {
 function createFail(error) {
 	return {
 		type: CREATE_POST_FAILED,
+	}
+}
+
+export function uploadFiles(files) {
+    return dispatch => {
+        axios.post(postsApp.baseUrl + postsAPI.uploadPhotos, files)
+		.then(function(res){
+			if (res && res.data && res.data.result) {
+				dispatch(uploadSuccess())
+			}
+		}).catch(function(err) {
+			dispatch(uploadFail(err))
+		})
+    }
+}
+
+function uploadSuccess() {
+	return {
+		type: UPLOAD_FILES_SUCCESSFUL,
+	}
+}
+
+function uploadFail(error) {
+	return {
+		type: UPLOAD_FILES_FAILED,
 	}
 }

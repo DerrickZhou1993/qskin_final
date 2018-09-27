d4e4ecea7d655a7194ed14393ff300db80d1ecf8
ting <Elin@inside-65-115-197.wireless.utoronto.ca>
2018-09-26 21:07:37 -0400

    change styling of HomeButton file

--- app/pages/common/HomeButton.jsx
+++ app/pages/common/HomeButton.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export default class HomeButton extends Component {
 	render() {
-	    return (<div className="container">	  
+		return (<div className="container">	  
 			<p><a href="/">Home</a></p>
 		</div>)
 	}

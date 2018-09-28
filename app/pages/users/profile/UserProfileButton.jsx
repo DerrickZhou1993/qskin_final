b43ada4ff4664c59611eaf1685a0564782f63edd
yuhaolu <yuhaolu0616@gmail.com>
2018-09-28 14:29:19 -0400

    Improve home button and user profile button css in the home page

--- app/pages/users/profile/UserProfileButton.jsx
+++ app/pages/users/profile/UserProfileButton.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import '../../../stylesheets/userProfileButton.scss'
+
 export default class UserProfileButton extends Component {
 
 	render() {
 		if (this.props.userSigninState.signedIn) {
-			return (<div className="container">
+			return (<div className="profileButtonContainer">
 				<div>
 					{<Link to="/users/me">
-						<button className="btn btn-primary" id="profileButton" type="button" >USER PROFILE</button>
+						<button className="btn btn-primary" id="profileButton" type="button" >MY PROFILE</button>
 					</Link>
 					}
 				</div>

473c74f6068e2ca1e8c712db44b564fc38e52c47
ting <Elin@WANGs-MacBook-Pro.local>
2018-09-29 00:51:17 -0400

    create class called centre

--- app/pages/homepage/Banner.jsx
+++ app/pages/homepage/Banner.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import banner from '../../assets/banner.png'
+import '../../stylesheets/banner.scss'
 
 export default class Banner extends Component {
 	render() {
 	  return (<div className="container">
-		  <img src={banner} alt="Qskin Forum"/>
+		  <img src={banner} alt="Qskin Forum" className="centre"/>
 		</div>)
 	}
 }

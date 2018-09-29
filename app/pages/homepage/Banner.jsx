6fa5032178a4c6ae7534a70c090445945517d30d
ting <Elin@WANGs-MacBook-Pro.local>
2018-09-28 23:28:34 -0400

    remove text on the banner

--- app/pages/homepage/Banner.jsx
+++ app/pages/homepage/Banner.jsx
@@ -1,14 +1,10 @@
 import React, { Component } from 'react'
-import '../../stylesheets/banner.scss'
 import banner from '../../assets/banner.png'
 
 export default class Banner extends Component {
 	render() {
 	  return (<div className="container">
-		  <img src={banner} alt="Qskin Forum"/>	
-			<div className="text">	  
-		    <p>You have not currently signed in. Please <a href="/users/signin">sign in from here</a>.</p>
-			</div>
+		  <img src={banner} alt="Qskin Forum"/>
 		</div>)
 	}
 }

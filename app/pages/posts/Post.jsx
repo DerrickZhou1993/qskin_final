b19e0df65370aa642f868e087fa731e6fcaf3aa7
ting <Elin@WANGs-MacBook-Pro.local>
2018-09-30 19:30:42 -0400

    use bootstrap to improve styling

--- app/pages/posts/Post.jsx
+++ app/pages/posts/Post.jsx
@@ -10,34 +10,36 @@ import bookmarkURL from '../../assets/icons/Bookmark.png'
 export default class Post extends Component {
 
 	render() {
-		return (<div className = "postcontainer">
+		return (<div className = "postcontainer col-sm-12">
 					<div className = "titleLine">		
-						<h2> {this.props.title} </h2>
-						<div className="dropdown">
-						<button className="dropbtn">Dropdown</button>
-						<div className="dropdown-content">
-							<a href="#">dropDown 1</a>
-							<a href="#">dropDown 2</a>
-							<a href="#">dropDown 3</a>
-						</div>
+						<h2 className= "col-sm-10"> {this.props.title} </h2>
+						<div className="dropdown col-sm-2">
+							<button className="dropbtn">...</button>
+								<div className="dropdown-content">
+									<a href="#">dropDown 1</a>
+									<a href="#">dropDown 2</a>
+									<a href="#">dropDown 3</a>
+								</div>
 						</div>
 					</div>	
-					<div className = "authorInfo">
-						<div> <img src={this.props.userAvatar} className="user-avatar" /> </div>
-						<div> {this.props.userName} </div>
-						<div> {this.props.time} </div>
+					<div className = "authorInfo col-sm-12">			
+						<img src={this.props.userAvatar} className="user-avatar" /> 
+						<div className="userName"> {this.props.userName} </div>
+						<div className="userTime"> {this.props.time} </div>	
 					</div>
 
-					<div> {this.props.content} </div>
-					<div> {this.props.phtotos} </div>
+					<div className="content col-sm-12"> {this.props.content} </div>
+					<div className="col-sm-12"> {this.props.phtotos} </div>
 
-					<div className="interaction">
-						<img src={likeURL} className="like-btn" />
-						<img src={commentURL} className="comment-btn" />
-						<div> {this.props.likeCount} </div>
+					<div className="col-sm-8"></div>
+					<div className="interaction col-sm-4">
 						<img src={bookmarkURL} className="reply-btn" />
-						<div> {this.props.replyCount} </div>
+						<img src={likeURL} className="like-btn" />
+						<div className="info"> {this.props.likeCount} </div>
+						<img src={commentURL} className="comment-btn" />
+						<div className="info"> {this.props.replyCount} </div>
 					</div>
+
 				</div>)
 	}
 }

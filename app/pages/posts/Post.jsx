ffbacb15a6d27f9cbcbbb863ab6f22ff83f94a15
ting <Elin@WANGs-MacBook-Pro.local>
2018-09-30 22:07:33 -0400

    remove extra space

--- app/pages/posts/Post.jsx
+++ app/pages/posts/Post.jsx
@@ -11,7 +11,7 @@ export default class Post extends Component {
 
 	render() {
 		return (<div className = "postcontainer col-sm-12">
-					<div className = "titleLine">		
+					<div className = "titleLine">
 						<h2 className= "col-sm-10"> {this.props.title} </h2>
 						<div className="dropdown col-sm-2">
 							<button className="dropbtn">...</button>
@@ -21,16 +21,14 @@ export default class Post extends Component {
 							<a href="#">Report this post</a>
 							</div>
 						</div>
-					</div>	
+					</div>
 					<div className = "authorInfo col-sm-12">
 						<img src={this.props.userAvatar} className="user-avatar" />
 						<div className="userName"> {this.props.userName} </div>
-						<div className="userTime"> {this.props.time} </div>	
+						<div className="userTime"> {this.props.time} </div>
 					</div>
-
 					<div className="content col-sm-12"> {this.props.content} </div>
 					<div className="col-sm-12"> {this.props.phtotos} </div>
-
 					<div className="col-sm-8"></div>
 					<div className="interaction col-sm-4">
 						<img src={bookmarkURL} className="reply-btn" />
@@ -39,7 +37,6 @@ export default class Post extends Component {
 						<img src={commentURL} className="comment-btn" />
 						<div className="info"> {this.props.replyCount} </div>
 					</div>
-
 				</div>)
 	}
 }

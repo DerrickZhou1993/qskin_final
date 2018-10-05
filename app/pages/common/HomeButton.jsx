2bae5112cd0da495c64ed5a5216b5d1d7fd4b10a
Dora Chien <dorachien@Chien-2.local>
2018-10-04 21:00:54 -0400

    use Link to link pages

--- app/pages/common/HomeButton.jsx
+++ app/pages/common/HomeButton.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import '../../stylesheets/homeButton.scss'
+import { Link } from 'react-router'
 
 export default class HomeButton extends Component {
 	render() {
 		return (<div className="homeLinkContainer">	  
-			<p id="homeLink"><a href="/">HOME</a></p>
+			<p id="homeLink">
+				<Link to='/'>HOME</Link>
+			</p>
 		</div>)
 	}
 }

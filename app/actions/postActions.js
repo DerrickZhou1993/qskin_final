9a1008834b5d039d3b89dd54562225c11faf0ad2
Dora Chien <dorachien@Chien-2.local>
2018-10-07 15:33:45 -0400

    fix space

--- app/actions/postActions.js
+++ app/actions/postActions.js
@@ -21,12 +21,12 @@ export function create(title, content, userId, filepath) {
 			}, {
 				headers: {user_id: readCookie(USER_ID_COOKIE)},
 			})
-			.then(function(res){
+			.then(function (res) {
 				if (res && res.data && res.data.result) {
 					dispatch(createSuccess())
 					dispatch(listposts())
 				}
-			}).catch(function(err) {
+			}).catch(function (err) {
 				dispatch(createFail(err))
 			})
 	}
